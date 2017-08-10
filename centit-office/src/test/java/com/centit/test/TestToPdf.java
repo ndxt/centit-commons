746107484674379def3b71f4c4a2b7e84a5a5a36
杨淮生 <codefan@centit.com>
2017-08-10 18:52:49 +0800

    添加office新特性，引入 xdocreport

--- centit-office/src/test/java/com/centit/test/TestToPdf.java
+++ centit-office/src/test/java/com/centit/test/TestToPdf.java
@@ -1,6 +1,13 @@
 package com.centit.test;
 
-import com.centit.support.office.OfficeToPdf;
+import fr.opensagres.poi.xwpf.converter.pdf.PdfConverter;
+import fr.opensagres.poi.xwpf.converter.pdf.PdfOptions;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 
 public class TestToPdf {
 	
@@ -10,7 +17,26 @@ public class TestToPdf {
 	 */
 	public static void main(String[] args) {
 
-		OfficeToPdf.office2Pdf("D:/temp/复星集团.doc", "D:/temp/复星集团.pdf");
+		//OfficeToPdf.office2Pdf("D:/temp/复星集团.doc", "D:/temp/复星集团.pdf");
+
+		try
+		{
+			// 1) Load docx with POI XWPFDocument
+			XWPFDocument document = new XWPFDocument( new FileInputStream( new File( "/D/temp/复星集团.docx")));
+
+			// 2) Convert POI XWPFDocument 2 PDF with iText
+			File outFile = new File( "/D/temp/复星集团.pdf" );
+			outFile.getParentFile().mkdirs();
+
+			OutputStream out = new FileOutputStream( outFile );
+			PdfOptions options = PdfOptions.create()/*.fontEncoding( "UTF-8" )*/;
+			PdfConverter.getInstance().convert( document, out, options );
+		}
+		catch ( Throwable e )
+		{
+			e.printStackTrace();
+		}
+
 	}
 
 }
