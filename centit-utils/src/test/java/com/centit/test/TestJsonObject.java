6f54544f28b5c53932bbd667a3cf6d4565a3e42c
杨淮生 <codefan@centit.com>
2017-08-10 11:07:45 +0800

    修改一个Bug

--- centit-utils/src/test/java/com/centit/test/TestJsonObject.java
+++ centit-utils/src/test/java/com/centit/test/TestJsonObject.java
@@ -2,14 +2,17 @@ package com.centit.test;
 
 import java.util.Map;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.centit.support.json.JSONOpt;
 
 public class TestJsonObject {
 
     public static void main(String arg[]){
-    	
-    	Map<String,Object> mp = JSONOpt.createHashMap("osid", 
+
+        System.out.println(JSON.toJSONString(new Object[]{5,6,"hello"}));
+
+        Map<String,Object> mp = JSONOpt.createHashMap("osid",
     			JSONOpt.createHashMap("type","string","index","analyzed"), 
     			"optid",
     			JSONOpt.createHashMap("type","string","index","analyzed"));
