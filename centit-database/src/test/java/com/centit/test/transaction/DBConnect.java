a381d65bbafb438b9e6b9c70079e5857963bc538
杨淮生 <codefan@centit.com>
2017-08-28 09:38:12 +0800

    重构，添加一个基于jpa注解的简单的单表Orm模块

--- centit-database/src/test/java/com/centit/test/transaction/DBConnect.java
+++ centit-database/src/test/java/com/centit/test/transaction/DBConnect.java
@@ -1,8 +1,10 @@
 package com.centit.test.transaction;
 
+import com.centit.support.algorithm.UuidOpt;
 import com.centit.support.database.utils.DBType;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 /**
  * 数据库连接信息，由于是使用的原生SQL(Native SQL)需要根据数据库类型做一些适应性操作，所有数据库类型信息
@@ -20,5 +22,18 @@ public interface DBConnect extends Connection{
 	boolean startTransaction();
 	
 	boolean isInTransaction();
-	
+
+	static DBConnect valueOf(Connection conn){
+		if( conn instanceof DBConnect){
+			return (DBConnect)conn;
+		}
+		String dataCode;
+		try {
+			dataCode = conn.getSchema();
+		} catch (SQLException e) {
+			dataCode = UuidOpt.getUuidAsString32();
+		}
+		DbcpConnect dbcpConn = new DbcpConnect(dataCode, conn );
+		return dbcpConn;
+	}
 }
