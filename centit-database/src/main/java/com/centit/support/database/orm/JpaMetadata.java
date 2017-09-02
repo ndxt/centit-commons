f30fb73fb4beb69f686140ecba8344e5e39e6503
zou_wy <zou_wy@centit.com>
2017-09-02 15:39:27 +0800

    修改一个判断错误

--- centit-database/src/main/java/com/centit/support/database/orm/JpaMetadata.java
+++ centit-database/src/main/java/com/centit/support/database/orm/JpaMetadata.java
@@ -43,7 +43,7 @@ public abstract class JpaMetadata {
 
     public static TableMapInfo obtainMapInfoFromClass(Class<?> objType){
 
-        if(objType.isAnnotationPresent( Table.class ))
+        if(!objType.isAnnotationPresent( Table.class ))
             return null;
         Table tableInfo = objType.getAnnotation(Table.class);
         TableMapInfo mapInfo = new TableMapInfo();
