666b0962e343656ce730f9f373d1fd2bccbcbd7e
杨淮生 <codefan@centit.com>
2017-09-07 09:47:29 +0800

    添加一个新的通用算法类，用于收录无法分类的算法

--- centit-utils/src/main/java/com/centit/support/common/GeneralOpt.java
+++ centit-utils/src/main/java/com/centit/support/common/GeneralOpt.java
@@ -7,6 +7,10 @@ package com.centit.support.common;
 public abstract  class GeneralOpt {
 
     public <T> T nvl(T obj, T obj2){
-        return obj==null?obj2:obj;
+        return obj==null ? obj2 : obj;
+    }
+
+    public <T> T nvl2(Object obj, T obj2, T obj3){
+        return obj==null ? obj3 : obj2;
     }
 }
