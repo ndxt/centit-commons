40f28e412739ca32802ccd9c3cf30f827696fc63
zou_wy <zou_wy@centit.com>
2017-09-11 18:29:46 +0800

    修改mysql sequence

--- centit-database/src/main/java/com/centit/support/database/jsonmaptable/MySqlJsonObjectDao.java
+++ centit-database/src/main/java/com/centit/support/database/jsonmaptable/MySqlJsonObjectDao.java
@@ -1,18 +1,18 @@
 package com.centit.support.database.jsonmaptable;
 
+import com.alibaba.fastjson.JSONArray;
+import com.centit.support.algorithm.NumberBaseOpt;
+import com.centit.support.database.metadata.TableInfo;
+import com.centit.support.database.utils.DatabaseAccess;
+import com.centit.support.database.utils.QueryUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
-
-import org.apache.commons.lang3.StringUtils;
-import org.apache.commons.lang3.tuple.Pair;
-
-import com.alibaba.fastjson.JSONArray;
-import com.centit.support.database.utils.DatabaseAccess;
-import com.centit.support.database.utils.QueryUtils;
-import com.centit.support.database.metadata.TableInfo;
 
 public class MySqlJsonObjectDao extends GeneralJsonObjectDao {
 	
@@ -55,11 +55,11 @@ public class MySqlJsonObjectDao extends GeneralJsonObjectDao {
 	
 	@Override
 	public Long getSequenceNextValue(final String sequenceName) throws SQLException, IOException {
-		return getSimulateSequenceNextValue(sequenceName);
-		/*Object object = DatabaseAccess.getScalarObjectQuery(
+		//return getSimulateSequenceNextValue(sequenceName);
+		Object object = DatabaseAccess.getScalarObjectQuery(
 				 getConnect(),
 				 "SELECT sequence_nextval ('"+ sequenceName+"')");
-		return NumberBaseOpt.castObjectToLong(object);*/
+		return NumberBaseOpt.castObjectToLong(object);
 	}
 	
 	@Override
