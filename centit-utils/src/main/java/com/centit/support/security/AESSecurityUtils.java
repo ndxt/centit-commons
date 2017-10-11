9bc66e73f69d65789cf6907553ee87be31452e5f
杨淮生 <codefan@centit.com>
2017-10-11 11:01:53 +0800

    解决 kgen.init(128, new SecureRandom(secret.getBytes())); 在windows下能够正常工作，在linux下有问题，

--- centit-utils/src/main/java/com/centit/support/security/AESSecurityUtils.java
+++ centit-utils/src/main/java/com/centit/support/security/AESSecurityUtils.java
@@ -7,6 +7,9 @@ import org.slf4j.LoggerFactory;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.GeneralSecurityException;
 import java.security.Key;
 import java.security.SecureRandom;
@@ -22,29 +25,39 @@ public abstract class AESSecurityUtils {
 
     public static Cipher createEncryptCipher(String keyValue) throws GeneralSecurityException  {
         Key key = getKey(keyValue);
-        Cipher encryptCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        Cipher encryptCipher = Cipher.getInstance("AES");//"AES/ECB/PKCS5Padding"
         encryptCipher.init(Cipher.ENCRYPT_MODE, key);
         return encryptCipher;
     }
 
     public static Cipher createDencryptCipher(String keyValue) throws GeneralSecurityException {
         Key key = getKey(keyValue);
-        Cipher decryptCipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+        Cipher decryptCipher = Cipher.getInstance("AES");//"AES/ECB/PKCS5Padding"
         decryptCipher.init(Cipher.DECRYPT_MODE, key);
         return decryptCipher;
     }
 
     /**
-     * 生成指定字符串的密钥   
+     * 生成指定字符串的密钥
+     * kgen.init(128, new SecureRandom(secret.getBytes())); 在windows下能够正常工作，在linux下有问题，
+     * SecureRandom 实现完全隨操作系统本身的內部狀態，
+     * 除非調用方在調用 getInstance 方法之後又調用了 setSeed 方法；
+     * 该实现在 windows 上每次生成的 key 都相同，但是在 solaris 或部分 linux 系统上则不同。
+     *
      * @param secret        要生成密钥的字符串   
      * @return secretKey    生成后的密钥   
      * @throws GeneralSecurityException 异常
      */    
-    private static Key getKey(String secret) throws GeneralSecurityException {     
-        KeyGenerator kgen = KeyGenerator.getInstance("AES");     
-        kgen.init(128, new SecureRandom(secret.getBytes()));     
-        SecretKey secretKey = kgen.generateKey();     
-        return secretKey;     
+    private static SecretKey getKey(String secret) throws GeneralSecurityException {
+        //1.构造密钥生成器，指定为AES算法,不区分大小写
+        KeyGenerator kgen = KeyGenerator.getInstance("AES");
+        SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+        secureRandom.setSeed(secret.getBytes());
+        //2.根据ecnodeRules规则初始化密钥生成器
+        //生成一个128位的随机源,根据传入的字节数组
+        kgen.init(128,secureRandom);
+        //3.产生原始对称密钥
+        return kgen.generateKey();
     }     
     
     /**
@@ -88,6 +101,8 @@ public abstract class AESSecurityUtils {
             return null;
         }
     }
+
+
     /**
      * 用base64解码再用Des解密
      * @param str 需解密的字节数组
