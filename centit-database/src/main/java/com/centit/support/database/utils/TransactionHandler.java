2c74dc010db809d5a7e99dcd89d98b13693e5ec0
杨淮生 <codefan@centit.com>
2017-11-08 08:46:00 +0800

    更新持久化包<dependency>         <groupId>javax.persistence</groupId>         <artifactId>javax.persistence-api</artifactId>         <version>2.2</version>       </dependency>

---
+++ centit-database/src/main/java/com/centit/support/database/utils/TransactionHandler.java
@@ -0,0 +1,36 @@
+package com.centit.support.database.utils;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public abstract class TransactionHandler {
+
+    private TransactionHandler() {
+        throw new IllegalAccessError("Utility class");
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(TransactionHandler.class);
+
+    public interface TransactionWork<T> {
+        T execute(Connection conn) throws SQLException;
+    }
+
+    public static <T> T executeInTransaction(DataSourceDescription dataSourceDesc, TransactionWork<T> realWork)
+        throws SQLException {
+        Connection conn = DbcpConnectPools.getDbcpConnect(dataSourceDesc);
+        try{
+            T relRet = realWork.execute(conn);
+            conn.commit();
+            return relRet;
+        } catch (SQLException e){
+            logger.error("error code :" + e.getSQLState() + e.getLocalizedMessage(),e);
+            conn.rollback();
+            throw e;
+        } finally {
+            DbcpConnectPools.closeConnect(conn);
+        }
+    }
+}
