d0f51b4ff7e1452ab587415df848a02819d58341
codefan <codefan@sina.com>
2017-11-09 13:30:27 +0800

    重构 castObjectToBoolean 方法

--- centit-utils/src/test/java/com/centit/test/TestTypeUtils.java
+++ centit-utils/src/test/java/com/centit/test/TestTypeUtils.java
@@ -1,10 +1,14 @@
 package com.centit.test;
 
+import com.centit.support.algorithm.BooleanBaseOpt;
 import com.centit.support.algorithm.NumberBaseOpt;
+import com.centit.support.algorithm.StringRegularOpt;
 
 public class TestTypeUtils {
 
     public static void main(String[] args) {
+        System.out.println(StringRegularOpt.isFalse("  "));
+        System.out.println(BooleanBaseOpt.castObjectToBoolean("  ", true));
         System.out.println(NumberBaseOpt.parseInteger("1", null));
         /*List<String> ls = new ArrayList<>();
         ls.add("hello");
