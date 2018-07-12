58800baf6adced9bfab90fcbaeb541da8687fd39
杨淮生 <codefan@centit.com>
2018-07-12 13:33:20 +0800

    添加 数学计算

--- centit-utils/src/main/java/com/centit/support/algorithm/Mathematics.java
+++ centit-utils/src/main/java/com/centit/support/algorithm/Mathematics.java
@@ -18,64 +18,70 @@ public class Mathematics {
     public static <T> void  combination(List<T> listSouce ,
                                  Comparator<? super T> comparable,
                                  Consumer<List<T>> consumer){
-
-        Collections.sort(listSouce, comparable);
+        if(listSouce.size()<2){
+            consumer.accept(listSouce);
+            return;
+        }
+        listSouce.sort(comparable);
         int len = listSouce.size();
         List<Integer> comPos = new ArrayList<>(len);
-        List<List<T>> subList = new ArrayList<>(len);
+        List<Boolean> usedItem = new ArrayList<>(len);
         List<T> comRes = new ArrayList<>(len);
 
         for(int i=0;i<len;i++){
             comPos.add(-1);
-            subList.add(new ArrayList<>(len));
+            usedItem.add(false);
             comRes.add(null);
         }
         comPos.set(0,0);
-        subList.set(0,listSouce);
-
         int sortIndex = 0;
-
+        usedItem.set(0, true);
         while(sortIndex >=0 ){
-            comRes.set(sortIndex, subList.get(sortIndex).get( comPos.get(sortIndex)));
+            comRes.set(sortIndex, listSouce.get( comPos.get(sortIndex)));
             if( sortIndex == len - 2){ // 如果获得一个排序
-                comRes.set( sortIndex +1,
-                        subList.get(sortIndex).get(
-                                (comPos.get(sortIndex) +1) % (len - sortIndex ) ) );
+                for(int i=0; i< len; i++){
+                    if(!usedItem.get(i)){// 将最后一个未使用的添加到排列的最后
+                        comRes.set( sortIndex +1, listSouce.get(i));
+                        break;
+                    }
+                }
                 consumer.accept(comRes);
-                //回退
+                //usedItem.set(comPos.get(sortIndex), false);
                 while(sortIndex >=0 ) {
-
-                    //当前pos ++
-                    while (comPos.get(sortIndex) + 1 < len - sortIndex && comparable.compare(
-                            subList.get(sortIndex).get(comPos.get(sortIndex)),
-                            subList.get(sortIndex).get(comPos.get(sortIndex) + 1)
-                    ) == 0) {
+                    //下一个
+                    int prePos = comPos.get(sortIndex);
+                    usedItem.set(prePos, false);
+                    //当前pos ++ （步进）
+                    while (comPos.get(sortIndex) + 1  < len &&
+                            ( usedItem.get(comPos.get(sortIndex) + 1) || comparable.compare(
+                                    listSouce.get(prePos),
+                                    listSouce.get(comPos.get(sortIndex) + 1)
+                    ) == 0 )) {
                         comPos.set(sortIndex, comPos.get(sortIndex) + 1);
                     }
 
                     comPos.set(sortIndex, comPos.get(sortIndex) + 1);
                     // 如果已经到上线，继续回退
-                    if (comPos.get(sortIndex)  < len - sortIndex) {
+                    if (comPos.get(sortIndex)  < len ) {
                         //重新计算下个列表
-                        subList.get(sortIndex + 1).clear();
-                        subList.get(sortIndex + 1).addAll(subList.get(sortIndex));
-                        subList.get(sortIndex + 1).remove( comPos.get(sortIndex).intValue());
-                        comPos.set(sortIndex + 1,0);
+                        usedItem.set(comPos.get(sortIndex), true);
+                        comRes.set( sortIndex, listSouce.get(comPos.get(sortIndex)));
                         break;
-                    }else{
+                    }else{ // 回退
                         sortIndex--;
                         //comPos.set(sortIndex, comPos.get(sortIndex) + 1);
                     }
                 }
-            }else {
-                subList.get(sortIndex + 1).clear();
-                subList.get(sortIndex + 1).addAll(subList.get(sortIndex));
-                subList.get(sortIndex + 1).remove( comPos.get(sortIndex).intValue());
-                comPos.set(sortIndex + 1,0);
+            } else { // 下一个
+                for(int i=0; i< len; i++){
+                    if(!usedItem.get(i)){
+                        comPos.set(sortIndex + 1,i);
+                        usedItem.set(i,true);
+                        break;
+                    }
+                }
                 sortIndex++;
             }
-
         }
-
     }
 }
--- centit-utils/src/test/java/com/centit/test/MathOpt.java
+++ centit-utils/src/test/java/com/centit/test/MathOpt.java
@@ -10,97 +10,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.BinaryOperator;
 
 /**
  * Created by codefan on 2017/11/3.
  * @author codefan
  */
 public class MathOpt {
-    //逆波兰式
-    static float calcReversePolishRepresentation (Object [] reversePolish ) {
-        float[] stack = new float[4];
-        int j = 0;
-        for (int i = 0; i < 7; i++) {
-            if (reversePolish[i] instanceof Integer) {
-                stack[j] =  ((Integer) reversePolish[i]).floatValue();
-                j++;
-            } else {
-                switch ((String) reversePolish[i]) {
-                    case "+":
-                        stack[j - 2] = stack[j - 2] + stack[j - 1];
-                        break;
-                    case "-":
-                        stack[j - 2] = stack[j - 2] - stack[j - 1];
-                        break;
-                    case "*":
-                        stack[j - 2] = stack[j - 2] * stack[j - 1];
-                        break;
-                    case "/":
-                        if (stack[j - 1] == 0) {
-                            return -1;
-                        }
-                        stack[j - 2] = stack[j - 2] / stack[j - 1];
-                        break;
-                }
-                j--;
-            }
-        }
-        return stack[0];
-    }
 
-    // 算24点
-    public static void calc24(Object [] reversePolish){
-        if( Math.abs(calcReversePolishRepresentation(reversePolish) - 24) < 0.0001f  ){
-            for(int i=0;i<7;i++){
-                System.out.print(reversePolish[i]);
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
-    }
 
     //将 数字和操作排序
     public static void sortFormulaOpt( List<Integer> rList ){
-        String[] opts = {"+","-","*","/"};
-        Object [] stack = new Object[7];
-        for(int i=0;i<4;i++){
-            for(int j=0;j<4;j++){
-                for(int k=0;k<4;k++){
-                    // a b + c + d +
-                    stack[0] = rList.get(0);
-                    stack[1] = rList.get(1);
-
-                    stack[2] = opts[i];
-                    stack[3] = rList.get(2);
-                    stack[4] = opts[j];
-                    stack[5] = rList.get(3);
-
-                    stack[6] = opts[k];
-                    calc24(stack);
-                    // a b + c d + +
-                    stack[3] = rList.get(2);
-                    stack[4] = rList.get(3);
-                    stack[5] = opts[j];
-                    calc24(stack);
-                    // a b c + d + +
-                    stack[2] = rList.get(2);
-                    stack[3] = opts[i];
-                    stack[4] = rList.get(3);
-                    calc24(stack);
-                    // a b c + + d +
-                    stack[3] = opts[i];
-                    stack[4] = opts[j];
-                    stack[5] = rList.get(3);
-                    calc24(stack);
-                    // a b c d + + +
-                    stack[3] = rList.get(3);
-                    stack[4] = opts[i];
-                    stack[5] = opts[j];
-                    calc24(stack);
-                }
-            }
+        for(Integer i : rList){
+            System.out.print(i);
+            System.out.print(" ");
         }
+        System.out.println();
 
     }
 
@@ -108,7 +32,7 @@ public class MathOpt {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         while (true) {
-            System.out.println("输入用空格隔开的4在1和10之间的整数，推出输入exit");
+            System.out.println("输入用空格隔开整数，推出输入exit");
             String s = br.readLine().trim();
             if(StringUtils.isBlank(s)){
                 continue;
@@ -121,13 +45,10 @@ public class MathOpt {
             for(int i=0; i<nums.length; i++){
                 if(StringRegularOpt.isNumber(nums[i])){
                     alist.add(NumberBaseOpt.castObjectToInteger(nums[i]));
-                    if( alist.size() == 4){
-                        break;
-                    }
                 }
             }
 
-            if( alist.size() < 4){
+            if( alist.size() < 1){
                 continue;
             }
 
