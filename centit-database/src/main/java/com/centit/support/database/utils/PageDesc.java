a28e86f3a12249587145249aaac8034bc1796831
杨淮生 <codefan@centit.com>
2018-08-09 15:50:22 +0800

    完善注释

--- centit-database/src/main/java/com/centit/support/database/utils/PageDesc.java
+++ centit-database/src/main/java/com/centit/support/database/utils/PageDesc.java
@@ -38,40 +38,76 @@ public class PageDesc {
         return totalRows;
     }
 
-    public void setTotalRows(int totalRows) {
-        this.totalRows = totalRows;
-    }
-
+    /**
+     * 设置条目总数
+     * @param totalRows Integer 用这个类型主要是为了应对查询总数的语句返回null的情况
+     */
     public void setTotalRows(Integer totalRows) {
         this.totalRows = totalRows==null ? 0 : totalRows;
     }
 
+    /**
+     * 获取每页最大条目数
+     * @return 每页最大条目数
+     */
     public int getPageSize() {
         return pageSize;
     }
 
+    /**
+     * 设置每页最大条目数
+     * @param pageSize 每页最大条目数
+     */
     public void setPageSize(int pageSize) {
         this.pageSize = pageSize;
     }
 
+    /**
+     * 应对遗留系统命名不一致的情况
+     * @param pageSize 每页数量
+     */
     public void setRows(int pageSize) {
         this.pageSize = pageSize;
     }
 
+    /**
+     * 获取当前页码
+     * @return 当前页码 base 1
+     */
     public int getPageNo() {
         return pageNo;
     }
 
+    /**
+     * 设置当前页码
+     * @param pageNo 当前页码 base 1
+     */
     public void setPageNo(int pageNo) {
         this.pageNo = pageNo;
     }
 
+    /**
+     * 应对遗留系统命名不一致的情况
+     * @param pageNo 当前页码 base 1
+     */
     public void setPage(int pageNo) {
         this.pageNo = pageNo;
     }
 
+    /**
+     * pageNo Base 1
+     * @return 当前页第一条记录index
+     */
     public int getRowStart() {
         return ( pageNo > 1 ? pageNo - 1 : 0 ) * pageSize;
+    }
+
+    /**
+     * pageNo Base 1
+     * @return 当前页最后一条记录index （不包含这条记录）
+     */
+    public int getRowEnd() {
+        return ( pageNo > 1 ? pageNo : 1 ) * pageSize;
     }
 
 }
