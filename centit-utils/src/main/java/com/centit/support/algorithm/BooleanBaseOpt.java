2ac24940756e19dd580981541dac0b781917625c
杨淮生 <codefan@centit.com>
2019-03-27 14:24:06 +0800

    添加常量

--- centit-utils/src/main/java/com/centit/support/algorithm/BooleanBaseOpt.java
+++ centit-utils/src/main/java/com/centit/support/algorithm/BooleanBaseOpt.java
@@ -2,6 +2,11 @@ package com.centit.support.algorithm;
 
 @SuppressWarnings("unused")
 public abstract class BooleanBaseOpt {
+    public static final String ONE_CHAR_TRUE = "T";
+    public static final String ONE_CHAR_FALSE = "T";
+    public static final String STRING_TRUE = "true";
+    public static final String STRING_FALSE = "false";
+
     private BooleanBaseOpt() {
         throw new IllegalAccessError("Utility class");
     }
