62b73c1a5438586e23536bafbe35aabb988b4696
codefan <codefan@sina.com>
2020-02-14 15:40:58 +0800

    修改 sql server 分页查询的bug

--- centit-database/src/main/java/com/centit/support/database/utils/QueryUtils.java
+++ centit-database/src/main/java/com/centit/support/database/utils/QueryUtils.java
@@ -645,7 +645,7 @@ public abstract class QueryUtils {
                         sqlLexer.getAWord(); // 获取数字 忽略
                         sw2 = sqlLexer.getAWord();
                     }
-                    sqlStr.append("top ").append(maxsize).append(" ").append(sw2);
+                    sqlStr.append("top ").append(maxsize).append(" ").append(sw2).append(" ");
                 } else {
                     sqlStr.append(sw).append(" ");
                 }
