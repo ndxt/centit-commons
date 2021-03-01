441fd02c3630fcecf48c83576a71ffab4b2e215f
codefan <codefan@sina.com>
2021-03-01 15:50:26 +0800

    添加常用方法

---
+++ centit-utils/src/main/java/com/centit/support/common/ListAppendMap.java
@@ -0,0 +1,31 @@
+package com.centit.support.common;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public class ListAppendMap<T> {
+    private List<T> listData;
+    private Map<String, T> appendMap;
+
+    public ListAppendMap(List<T> listData, Function<T, String> func){
+        if(listData!=null){
+            this.listData = listData;//Collections.unmodifiableList(listData);
+            //this.appendMap = new HashMap<>();
+            this.appendMap = new HashMap<>(listData.size());
+            for(T d : listData){
+                this.appendMap.put(func.apply(d), d);
+            }
+            //this.appendMap = Collections.unmodifiableMap(this.appendMap);
+        }
+    }
+
+    public List<T> getListData() {
+        return listData;
+    }
+
+    public Map<String, T> getAppendMap() {
+        return appendMap;
+    }
+}
